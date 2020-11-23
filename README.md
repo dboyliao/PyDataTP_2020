51332bd067165fa97005e5d7c1ad6d9404545e27
dboyliao <qmalliao@gmail.com>
2020-11-23 15:43:50 +0800

    update README and example code

---
+++ README.md
@@ -0,0 +1,18 @@
+# Requirements
+
+- `clang++` or any compiler that support [`C++11`](https://en.wikipedia.org/wiki/C%2B%2B11)
+- `cmake` with version `>= 3.10`
+
+# How to Compile
+
+```bash
+$ git clone https://github.com/dboyliao/PyDataTP_2020.git
+$ cd PyDataTP_2020 && git submodule update --init
+$ mkdir build
+$ cd build && cmake -DCMAKE_BUILD_TYPE=Debug ..
+$ make hello_world
+```
+
+Run `hello_world/hello_world` under `build` directory:
+
+![hello-world](images/hello_world.png)
--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -8,7 +8,7 @@ const int32_t msg_codes[] = {72, 101, 108, 108, 111, 44,
                              32, 119, 111, 114, 108, 100};
 const uint16_t num_codes = sizeof(msg_codes) / sizeof(msg_codes[0]);
 static localCircularArenaAllocator<512> meta_alloc;
-static localCircularArenaAllocator<sizeof(int32_t) * num_codes> ram_alloc;
+static localCircularArenaAllocator<sizeof(float) * num_codes> ram_alloc;
 
 int main(int argc, const char **argv) {
   Context::get_default_context()->set_metadata_allocator(&meta_alloc);
@@ -22,9 +22,9 @@ int main(int argc, const char **argv) {
   }
   printf("\n");
   // writing to tensor
-  Tensor ram_msg_tensor = new RamTensor({num_codes}, i32);
+  Tensor ram_msg_tensor = new RamTensor({num_codes}, flt);
   for (int i = 0; i < num_codes; ++i) {
-    ram_msg_tensor(i) = static_cast<int32_t>(msg_tensor(i));
+    ram_msg_tensor(i) = static_cast<float>(msg_tensor(i));
   }
   printf("Reading ram tensor: ");
   for (int i = 0; i < num_codes; ++i) {
